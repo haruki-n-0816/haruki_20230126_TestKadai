a2704aacac570a8ce5b51d45f214da75117ea4b1
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-01-27 16:39:40 +0900

    スレッド問題追加

---
+++ ThreadBasic.java
@@ -0,0 +1,21 @@
+
+class ThraedBasic {
+    public static void main(String[] args) {
+        var th1 = new MyThread();
+        var th2 = new MyThread();
+        var th3 = new MyThread();
+
+        th1.start();
+        th2.start();
+        th3.start();
+    }
+}
+
+class MyThread extends Thread {
+    @Override
+    public void run() {
+        for (int i = 0; i < 30; i++) {
+            System.out.println(this.getName() + ":" + i);
+        }
+    }
+}
