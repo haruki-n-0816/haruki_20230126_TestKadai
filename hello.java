51afb9f0a3f9fa649a86fc2192ebe5385ec07b38
haruki-n-0816 <kaihatsu.haruki.nagasawa@gmail.com>
2023-01-27 14:34:58 +0900

    ラムダ式追加

--- hello.java
+++ hello.java
@@ -1,5 +1,8 @@
 class hello{
     public static void main(String[] args) {
         System.out.println("hello");
+
+        Runnable runner = () -> { System.out.println("Hello Lambda!"); };
+        runner.run(); //Hello Lambda!
     }
 }
